14b06ee82215a67fa22b06ada212e08d506c1ee1
Hemru Chamuditha <chamudhitha123@gmail.com>
2023-03-02 15:35:42 +0530

    RMI practise

---
+++ src/rmi/RemoteInterface.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author Hemru
+ */
+public interface RemoteInterface extends Remote{
+    public void remoteMethod() throws RemoteException;
+    
+}
---
+++ src/server/RemoteClass.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import rmi.RemoteInterface;
+
+/**
+ *
+ * @author Hemru
+ */
+public class RemoteClass extends UnicastRemoteObject implements RemoteInterface{
+
+    public RemoteClass() throws RemoteException{
+        
+    }
+    
+    @Override
+    public void remoteMethod() throws RemoteException {
+        System.out.println("Client called this method");
+    }
+    
+}
